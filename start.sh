6e009e778e29f0031ea8afa442a360638ee7a403
gotcute5 <80556982+gotcute5@users.noreply.github.com>
2021-03-12 17:57:24 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q7vzc99supwgydr6kk5trcx6fcwnh4safw7vgdz.test1
 
 
 done
